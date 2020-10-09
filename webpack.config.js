04346e2a102438cb56c9ebfd2af6facffc3b4584
affinehat <krs.20xx@gmail.com>
2020-10-09 01:13:15 -0400

    nit: minor cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require("path");
+const path = require('path');
 const webpack = require('webpack');
-const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
+const PnpWebpackPlugin = require('pnp-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader"
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 1,
-	    }
+            },
           },
-          "postcss-loader",
-        ]
+          'postcss-loader',
+        ],
       },
-    ]
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new ReactRefreshWebpackPlugin(),
   ],
   resolve: {
-    extensions: ["*", ".js", ".jsx"],
+    extensions: ['*', '.js', '.jsx'],
     plugins: [ PnpWebpackPlugin ],
   },
   resolveLoader: {
@@ -42,13 +42,14 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/assets/",
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/',
+    filename: 'bundle.js',
   },
   devServer: {
     port: 3000,
-    contentBase: path.resolve(__dirname, "public"),
+    contentBase: path.resolve(__dirname, 'public'),
     hotOnly: true,
   },
 };
+
