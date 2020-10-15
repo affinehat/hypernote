ceeb7b1357599c24d03cbcda75c109ea491d4d4a
affinehat <krs.20xx@gmail.com>
2020-10-15 17:07:53 -0400

    localstorage persistance

--- src/store.js
+++ src/store.js
@@ -1,9 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { from } from 'rxjs'
 
 import notesReducer from 'notes/notesSlice'
 
-export default configureStore({
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('redux-state')
+    if (serializedState == null) { return undefined }
+    return JSON.parse(serializedState)
+  } catch (err) {
+    console.error(err)
+    return undefined
+  }
+}
+
+const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('redux-state', serializedState)
+  } catch (err) {
+    console.err(err)
+  }
+}
+
+const preloadedState = loadState()
+const store = configureStore({
   reducer: {
     notes: notesReducer,
   },
+  preloadedState
 })
+
+const store$ = from(store)
+store$.subscribe(next => {
+  saveState(store.getState())
+})
+
+export default store
