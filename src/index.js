7a4ebe965f7654ced3d6fb63f6b9a1561be31007
affinehat <krs.20xx@gmail.com>
2020-10-15 23:11:24 -0400

    render app with react-router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import App from './App.js'
 import store from './store'
-import { Provider } from 'react-redux'
 import './css/tailwind.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Route path="/" component={App} />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('react-root')
