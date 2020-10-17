170f95302624d12a3afb66b91ff02c639ea98e58
affinehat <krs.20xx@gmail.com>
2020-10-16 20:03:29 -0400

    render markdown in editor

--- src/notes/noteEditor.js
+++ src/notes/noteEditor.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import ReactMarkdown from 'react-markdown/with-html'
+import Frame from 'react-frame-component'
 
 export const NoteEditor = props => {
+  const testSource = '# Hello World'
   return (
     <div className="grid grid-cols-2">
-      <div>{props.note.text}</div>
-      <div>"output"</div>
+      <div id="editor-panel">{ props.note.text || testSource }</div>
+      <div id="result-panel" className="bg-white">
+        <Frame className="min-h-screen min-w-full" head={
+          <link type='text/css' rel='stylesheet'
+            href='https://www.gitcdn.xyz/repo/markdowncss/splendor/master/css/splendor.min.css' />
+        }>
+          <ReactMarkdown source={testSource} escapeHtml={false} />
+        </Frame>
+      </div>
     </div>
   )
 }
